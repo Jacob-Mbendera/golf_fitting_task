c7dc4af658c0fbec67fb58728696fdcab4bf68b5
Omega Msiska <35676707+mitosisX@users.noreply.github.com>
2024-10-08 14:21:23 +0200

    .

--- admin_fitting/urls.py
+++ admin_fitting/urls.py
@@ -4,12 +4,14 @@ from .views import AdminFittingViewSet
 from .views import CustomTokenObtainPairView
 from rest_framework.routers import DefaultRouter
 from consumer.views import ProfileViewSet
+from .views import GettingStartedInfoDetailView
 
 router = DefaultRouter()
 router.register(r'profiles', ProfileViewSet, basename='profile')
 
 urlpatterns = [
     path('login', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('fittings/', AdminFittingViewSet.as_view({'get': 'list'}), name='fitting-list'),
+    path('fittings', AdminFittingViewSet.as_view({'get': 'list'}), name='fitting-list'),
+    path('getting-started-info', GettingStartedInfoDetailView.as_view(), name='getting-started-info'),
     path('', include(router.urls)),
 ]
